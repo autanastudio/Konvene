e86e73ee3cf8deb39da89828b778bc985b813d0f
Alexey Belezeko <alexey.belezeko@sfcd.com>
2015-03-11 16:40:47 +0300

    Add login and account managers

---
+++ Klike/Sources/Model/KLAccountManager.h
@@ -0,0 +1,19 @@
+//
+//  KLAccountManager.h
+//  Klike
+//
+//  Created by admin on 11/03/15.
+//  Copyright (c) 2015 SFÇD, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KLAccountManager : NSObject
+
+@property(nonatomic, strong) PFUser *currentUser;
+
++ (instancetype)sharedManager;
+
+- (BOOL)isCurrentUserAuthorized;
+
+@end
---
+++ Klike/Sources/Model/KLLoginManager.m
@@ -0,0 +1,96 @@
+//
+//  KLLoginManager.m
+//  Klike
+//
+//  Created by admin on 11/03/15.
+//  Copyright (c) 2015 SFÇD, LLC. All rights reserved.
+//
+
+#import "KLLoginManager.h"
+#import "KLAccountManager.h"
+
+@interface KLLoginManager ()
+
+@end
+
+static NSString *klRequestCodeCloudeFunctionName = @"requestCode";
+static NSString *klAuthorizeCloudeFunctionName = @"authorize";
+
+static NSString *klUserPhoneNumberKey = @"phoneNumber";
+static NSString *klUserVerificationCodeKey = @"verificationCode";
+
+static NSString *klDefaultCountryCode = @"+1";
+
+@implementation KLLoginManager
+
++ (instancetype)sharedManager {
+    static KLLoginManager *sharedMyManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedMyManager = [[self alloc] init];
+    });
+    return sharedMyManager;
+}
+
+- (instancetype)init
+{
+    if (self = [super init]) {
+        self.countryCode = klDefaultCountryCode;
+        self.phoneNumber = nil;
+        self.verificationCode = nil;
+    }
+    return self;
+}
+
+- (void)requestAuthorizationWithHandler:(void (^)(BOOL success, NSError *error))completiotion
+{
+    [self requestAuthorizationWithPhoneNumber:[self getFullPhoneNumberString]
+                                      handler:completiotion];
+}
+
+- (void)authorizeUserWithHandler:(void (^)(PFUser *user, NSError *error))completiotion
+{
+    [self authorizeUserWithPhoneNumber:[self getFullPhoneNumberString]
+                      verificationCode:self.verificationCode
+                               handler:completiotion];
+}
+
+- (void)requestAuthorizationWithPhoneNumber:(NSString *)phoneNumber
+                                    handler:(void (^)(BOOL, NSError *))completiotion
+{
+    [PFCloud callFunctionInBackground:klRequestCodeCloudeFunctionName
+                       withParameters:@{ klUserPhoneNumberKey : phoneNumber }
+                                block:^(id object, NSError *error) {
+                                    completiotion(!error, error);
+                                }];
+}
+
+- (void)authorizeUserWithPhoneNumber:(NSString *)phoneNumber
+                    verificationCode:(NSString *)code
+                             handler:(void (^)(PFUser *, NSError *))completiotion
+{
+    [PFCloud callFunctionInBackground:klAuthorizeCloudeFunctionName
+                       withParameters:@{ klUserPhoneNumberKey : phoneNumber,
+                                         klUserVerificationCodeKey : code}
+                                block:^(id object, NSError *error) {
+                                    if (!error) {
+                                        [PFUser becomeInBackground:@"" block:^(PFUser *user, NSError *error) {
+                                            if (!error) {
+                                                [KLAccountManager sharedManager].currentUser = user;
+                                                completiotion(user, nil);
+                                            } else {
+                                                completiotion(nil, error);
+                                            }
+                                        }];
+                                    } else {
+                                        completiotion(nil, error);
+                                    }
+                                }];
+}
+
+- (NSString *)getFullPhoneNumberString
+{
+    return [self.countryCode stringByAppendingString:self.phoneNumber];
+}
+
+@end
