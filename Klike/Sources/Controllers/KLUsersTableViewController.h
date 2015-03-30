0d102ee7a2dc0ef751cc4e0d70f92532b82a115e
dmitry alexandrov <dmitry.alexandrov@softfacade.com>
2015-03-30 18:01:40 +0400

    Added KLUsersTableViewController, need to customize it later

---
+++ Klike/Sources/Controllers/KLUsersTableViewController.h
@@ -0,0 +1,21 @@
+//
+//  KLUsersTableViewController.h
+//  Klike
+//
+//  Created by Дмитрий Александров on 30.03.15.
+//  Copyright (c) 2015 SFÇD, LLC. All rights reserved.
+//
+#import "PFQueryTableViewController.h"
+#import "KLUserWrapper.h"
+
+typedef NS_ENUM(NSInteger, KLUserListType){
+    KLUserListTypeBoth,
+    KLUserListTypeFollowers,
+    KLUserListTypeFollowing
+};
+
+@interface KLUsersTableViewController : PFQueryTableViewController
+
+- (instancetype) initWithUser:(KLUserWrapper *)user type:(KLUserListType) type;
+
+@end
