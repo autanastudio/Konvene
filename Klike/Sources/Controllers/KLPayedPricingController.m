c003c4e22ee656901dd9247c408fb717077bc09f
AlexeyBelezeko <alexey.belezeko@sfcd.com>
2015-05-28 18:56:34 +0300

    Fix pricing layouts KLK-175

--- Klike/Sources/Controllers/KLPayedPricingController.m
+++ Klike/Sources/Controllers/KLPayedPricingController.m
@@ -10,19 +10,31 @@
 
 @interface KLPayedPricingController ()
 
+@property (nonatomic, strong) UIScrollView *scrolleView;
+
 @end
 
 @implementation KLPayedPricingController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    
+    [super viewDidLoad];
+    self.scrolleView = [[UIScrollView alloc] init];
+    [self.view addSubview:self.scrolleView];
+    [self.scrolleView autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsZero];
+    self.scrolleView.alwaysBounceVertical = YES;
+    self.scrolleView.keyboardDismissMode = UIScrollViewKeyboardDismissModeOnDrag;
+    
     UINib *nib = [UINib nibWithNibName:@"PayedPricingView" bundle:nil];
     self.pricingView = [nib instantiateWithOwner:nil
                                          options:nil].firstObject;
-    [self.view addSubview:self.pricingView];
     self.pricingView.processing = 0.02;//TODO get value from server
-    [self.pricingView autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsZero
-                                               excludingEdge:ALEdgeBottom];
+    CGSize screenSize = [UIScreen mainScreen].bounds.size;
+    [self.scrolleView addSubview:self.pricingView];
+    [self.pricingView autoSetDimension:ALDimensionWidth toSize:screenSize.width];
+    [self.pricingView autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsZero];
 }
 
 - (NSString *)title
--- Klike/Sources/Controllers/KLThrowPricingController.m
+++ Klike/Sources/Controllers/KLThrowPricingController.m
@@ -10,18 +10,27 @@
 
 @interface KLThrowPricingController ()
 
+@property (nonatomic, strong) UIScrollView *scrolleView;
+
 @end
 
 @implementation KLThrowPricingController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.scrolleView = [[UIScrollView alloc] init];
+    [self.view addSubview:self.scrolleView];
+    [self.scrolleView autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsZero];
+    self.scrolleView.alwaysBounceVertical = YES;
+    self.scrolleView.keyboardDismissMode = UIScrollViewKeyboardDismissModeOnDrag;
+    
     UINib *nib = [UINib nibWithNibName:@"ThrowPricingView" bundle:nil];
     self.pricingView = [nib instantiateWithOwner:nil
                                           options:nil].firstObject;
-    [self.view addSubview:self.pricingView];
-    [self.pricingView autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsZero
-                                               excludingEdge:ALEdgeBottom];
+    CGSize screenSize = [UIScreen mainScreen].bounds.size;
+    [self.scrolleView addSubview:self.pricingView];
+    [self.pricingView autoSetDimension:ALDimensionWidth toSize:screenSize.width];
+    [self.pricingView autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsZero];
 }
 
 - (NSString *)title
